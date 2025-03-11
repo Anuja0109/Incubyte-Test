4e5f164e52290d43843cc5500e5649d79b55a62a
Anuja Shrivastav <https://Anuja0109@github.com>
2025-03-11 17:37:28 +0530

    Tests for checking addNumbersOnly function is working or not

--- src/components/StringCalculator/StringCalculator.spec.tsx
+++ src/components/StringCalculator/StringCalculator.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, within } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import StringCalculator from ".";
 import addNumbersOnly from "../../utils/addNumbersOnly";
@@ -21,5 +21,19 @@ describe("StringCalculator component", () => {
   it("For checking if addNumbersOnly function is working with empty string", () => {
     const sum = addNumbersOnly("");
     expect(sum).toBe(0);
-  })
+  });
+
+  it("For checking if addNumbersOnly function is working with valid string & rendering correctly in StringCalculator component", () => {
+    const sum = addNumbersOnly("2,3");
+    expect(sum).toBe(5);
+
+    render(<StringCalculator />);
+    const { getByText } = within(screen.getByTestId("display-sum"));
+    expect(getByText("5")).toBeInTheDocument();
+  });
+
+  it("For checking addNumbersOnly function is working with valid input having several different delimeters", () => {
+    const sum = addNumbersOnly("2,;'3");
+    expect(sum).toBe(5);
+  });
 });
